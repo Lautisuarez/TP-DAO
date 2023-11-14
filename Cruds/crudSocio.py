24befffaf03e6839bda3a8115e82ac66aceb3917
Ro <rorodriguez1306@gmail.com>
2023-11-13 21:05:00 -0300

    me falto este

--- Cruds/crudSocio.py
+++ Cruds/crudSocio.py
@@ -8,9 +8,9 @@ def agregar_socio(socio: Socio):
     with sqlite3.connect('bd.db') as conexion:
         cursor = conexion.cursor()
         cursor.execute('''
-            INSERT INTO socios (codigo, nombre, libros_prestados)
-            VALUES (?, ?, ?)
-        ''', (socio.get_codigo(), socio.get_nombre(), socio.get_libros_prestados()))
+            INSERT INTO socios (dni, nombre, apellido, libros_prestados)
+            VALUES (?, ?, ?, ?)
+        ''', (socio.get_dni(), socio.get_nombre(), socio.get_apellido(), socio.get_libros_prestados()))
 
 def obtener_socios():
     with sqlite3.connect('bd.db') as conexion:
@@ -26,14 +26,14 @@ def actualizar_socio(socio: Socio):
         cursor = conexion.cursor()
         cursor.execute('''
             UPDATE socios
-            SET nombre = ?, libros_prestados = ?
-            WHERE codigo = ?
-        ''', (socio.get_nombre(), socio.get_libros_prestados(), socio.get_codigo()))
+            SET nombre = ?, apellido = ? libros_prestados = ?
+            WHERE dni = ?
+        ''', (socio.get_nombre(), socio.get_apellido(), socio.get_libros_prestados(), socio.get_dni()))
 
-def eliminar_socio(codigo: int):
+def eliminar_socio(dni: int):
     with sqlite3.connect('bd.db') as conexion:
         cursor = conexion.cursor()
         cursor.execute('''
             DELETE FROM socios
-            WHERE codigo = ?
-        ''', (codigo,))
+            WHERE dni = ?
+        ''', (dni,))
